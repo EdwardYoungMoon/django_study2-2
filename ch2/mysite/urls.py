dd228895850d9952764790be2a84eced957b1445
edwardmoon <jmoon0227@gmail.com>
2022-07-08 18:53:30 +0900

    homepage

--- ch2/mysite/urls.py
+++ ch2/mysite/urls.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from bookmark.views import BookmarkDV, BookmarkLV
+# from bookmark.views import BookmarkDV, BookmarkLV
+
+from mysite.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('bookmark/', include('bookmark.urls')),
     path('blog', include('blog.urls')),
+
+    path('', HomeView.as_view(), name='home')
 
     # # class-based views
     # path('bookmark/', BookmarkLV.as_view(), name='index'),
